f45fdf31ec075631c9efd61043a187bcda719047
Jetro Bagasala <jethgen.26@gmail.com>
2023-02-20 16:00:03 +0800

    Update EventNav.vue

--- src/components/nav/EventNav.vue
+++ src/components/nav/EventNav.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="text-left">
+    <div class="text-left mx-4">
         <v-menu transition="scroll-y-transition">
             <template v-slot:activator="{ props }">
                 <v-btn 
-                    variant="button"
+                    variant="text"
                     class="ma-2"
                     v-bind="props"
                     prepend-icon="mdi-vuetify"
@@ -13,10 +13,10 @@
             </template>
         </v-menu>
     </div>
-    <div class="text-left">
+    <div class="text-left mx-4">
         <v-menu transition="scroll-y-transition">
             <template v-slot:activator="{ props }">
-                <v-btn
+                <v-btn 
                     variant="text"
                     class="ma-2"
                     v-bind="props"
@@ -41,7 +41,7 @@
             </v-list>
         </v-menu>
     </div>
-    <div class="text-left">
+    <div class="text-left mx-4">
         <v-menu transition="scroll-y-transition">
             <template v-slot:activator="{ props }">
                 <v-btn
@@ -55,24 +55,21 @@
             </template>
             <v-list>
                 <v-list-item link>
-                    <v-list-item-title v-text="'Event_Name'"></v-list-item-title>
+                    <v-list-item-title v-text="'VOCAL SOLO (MALE)'"></v-list-item-title>
                 </v-list-item>
                 <v-list-item link>
-                    <v-list-item-title v-text="'Event_Name'"></v-list-item-title>
+                    <v-list-item-title v-text="'VOCAL SOLO (FEMALE)'"></v-list-item-title>
                 </v-list-item>
                 <v-list-item link>
-                    <v-list-item-title v-text="'Event_Name'"></v-list-item-title>
+                    <v-list-item-title v-text="'VOCAL DUET'"></v-list-item-title>
                 </v-list-item>
                 <v-list-item link>
-                    <v-list-item-title v-text="'Event_Name'"></v-list-item-title>
-                </v-list-item>
-                <v-list-item link>
-                    <v-list-item-title v-text="'Event_Name'"></v-list-item-title>
+                    <v-list-item-title v-text="'ACOUSTIC BAND'"></v-list-item-title>
                 </v-list-item>
             </v-list>
         </v-menu>
     </div>
-    <div class="text-left">
+    <div class="text-left mx-4">
         <v-menu transition="scroll-y-transition">
             <template v-slot:activator="{ props }">
                 <v-btn
@@ -82,6 +79,93 @@
                     prepend-icon="mdi-vuetify"
                 >
                     Dance
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item link>
+                    <v-list-item-title v-text="'JAZZ'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'FOLK DANCE'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'HIPHOP'"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+    <div class="text-left mx-4">
+        <v-menu transition="scroll-y-transition">
+            <template v-slot:activator="{ props }">
+                <v-btn
+                    variant="text"
+                    class="ma-2"
+                    v-bind="props"
+                    prepend-icon="mdi-vuetify"
+                >
+                    Ball Games
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item link>
+                    <v-list-item-title v-text="'BASKETBALL'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'VOLLEYBALL'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'TABLE TENNIS'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'BADMINTON'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'SEPAK TAKRAW'"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+    <div class="text-left mx-4">
+        <v-menu transition="scroll-y-transition">
+            <template v-slot:activator="{ props }">
+                <v-btn
+                    variant="text"
+                    class="ma-2"
+                    v-bind="props"
+                    prepend-icon="mdi-vuetify"
+                >
+                    Board Games
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item link>
+                    <v-list-item-title v-text="'CHESS'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'SCIDAMA'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'GAMES OF THE GENERAL'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'WORD FACTORY'"></v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-title v-text="'SUNGKA'"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+    <div class="text-left mx-4">
+        <v-menu transition="scroll-y-transition">
+            <template v-slot:activator="{ props }">
+                <v-btn
+                    variant="text"
+                    class="ma-2"
+                    v-bind="props"
+                    prepend-icon="mdi-vuetify"
+                >
+                    Athletics
                 </v-btn>
             </template>
             <v-list>
@@ -104,6 +188,7 @@
         </v-menu>
     </div>
 </template>
+
 
 <script>
     export default {
