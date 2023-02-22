8307b7c6be8fda4ab7f97af72a049aa1c97498b0
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-22 10:33:57 +0800

    Update SideNav.vue

--- src/components/nav/SideNav.vue
+++ src/components/nav/SideNav.vue
@@ -17,7 +17,6 @@
 </template>
 
 <script>
-import $ from "jquery";
 import eventNav from "./EventNav.vue";
 
 export default {
@@ -27,34 +26,11 @@ export default {
 	},
 	data() {
 		return {
-			dialog: false,
-			signedOut: false,
-			avatar: `${import.meta.env.BASE_URL}no-avatar.jpg`,
-			appName: `${this.$store.getters.appName}`,
-			judgeName: 'JUDGE'
+
 		}
 	},
 	methods: {
-		signOut() {
-			$.ajax({
-				url: `${this.$store.getters.appURL}/index.php`,
-				type: 'POST',
-				xhrFields: {
-					withCredentials: true
-				},
-				data: {
-					signOut: this.signedOut
-				},
-				success: (data) => {
-					data = JSON.parse(data);
-					this.$store.commit('auth/setUser', data.user = null);
-					this.$router.push('/');
-				},
-				error: (error) => {
-					alert(`ERROR ${error.status}: ${error.statusText}`);
-				},
-			})
-		}
+
 	}
 }
 </script>
