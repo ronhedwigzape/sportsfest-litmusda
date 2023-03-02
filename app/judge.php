789f48a04f334645483096860732fc9d8a3bd255
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-03-02 18:00:00 +0800

    Update judge.php

--- app/judge.php
+++ app/judge.php
@@ -38,7 +38,6 @@ else {
                 'criteria' => $event->getRowCriteria(),
                 'teams'    => $event->getRowTeams(),
                 'ratings'  => $judge->getRowEventRatings($event)
-                'ranks'    => $judge->getEventRanks($event)
             ]);
         }
 
