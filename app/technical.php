dbee474f04e8d2a958b34d5e36165c23e43906a3
kulotsystems <kulotsystems@gmail.com>
2023-03-11 09:32:57 +0800

    Categorize events in sidebar.

--- app/technical.php
+++ app/technical.php
@@ -4,65 +4,68 @@ require_once '_init.php';
 // get authenticated user
 $authUser = getUser();
 
-if (!$authUser)
+if(!$authUser)
     denyAccess();
 
-else if ($authUser['userType'] !== 'technical')
+else if($authUser['userType'] !== 'technical')
     denyAccess();
 
 else {
     require_once 'models/Technical.php';
     $technical = new Technical($authUser['username'], $_SESSION['pass']);
 
-    if (!$technical->authenticated())
+    if(!$technical->authenticated())
         denyAccess();
 
     else {
 
         // get events assigned to technical
-        if (isset($_GET['getEvents'])) {
+        if(isset($_GET['getEvents'])) {
+            require_once 'models/Category.php';
+
             echo json_encode([
-                "events" => $technical->getRowEvents()
+                'categories' => Category::rows(),
+                'events'     => $technical->getRowEvents()
             ]);
         }
 
         // get deduction sheet of the passed event
-        else if (isset($_GET['getDeductionSheet'])) {
+        else if(isset($_GET['getDeductionSheet'])) {
             require_once 'models/Event.php';
 
             $event_slug = trim($_GET['getDeductionSheet']);
-            $event    = Event::findBySlug($event_slug);
+            $event = Event::findBySlug($event_slug);
 
             echo json_encode([
-                'event'    => $event->toArray(),
-                'teams'    => $event->getRowTeams(),
-                'deductions'  => $technical->getRowEventDeductions($event)
+                'event'      => $event->toArray(),
+                'teams'      => $event->getRowTeams(),
+                'deductions' => $technical->getRowEventDeductions($event)
             ]);
         }
 
         // auto save deductions for teams
-        else if (isset($_POST['deduction'])) {
+        else if(isset($_POST['deduction'])) {
             require_once 'models/Deduction.php';
             require_once 'models/Event.php';
             require_once 'models/Team.php';
 
             $deduction = $_POST['deduction'];
 
-                $technical->setEventTeamDeduction(
-                    Event::findById($deduction['event_id']),
-                    Team::findById($deduction['team_id']),
-                    floatval($deduction['value'])
-                );
+            $technical->setEventTeamDeduction(
+                Event::findById($deduction['event_id']),
+                Team::findById($deduction['team_id']),
+                floatval($deduction['value'])
+            );
 
         }
 
         // set is_locked deductions to true
-        else if (isset($_POST['deductions'])) {
+        else if(isset($_POST['deductions'])) {
             require_once 'models/Deduction.php';
             require_once 'models/Event.php';
             require_once 'models/Team.php';
 
-            foreach ($_POST['deductions'] as $deduction) {
+            foreach($_POST['deductions'] as $deduction) {
                 $technical->setEventTeamDeduction(
                     Event::findById($deduction['event_id']),
                     Team::findById($deduction['team_id']),
