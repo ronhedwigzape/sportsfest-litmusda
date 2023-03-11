996f9087a6fb016fbe36d5677247a7eef08eacf3
kulotsystems <kulotsystems@gmail.com>
2023-03-11 23:07:11 +0800

    Create view for overall results.

---
+++ app/results/overall/index.php
@@ -0,0 +1,121 @@
+<?php
+    require_once '../../config/database.php';
+
+     require_once '../../models/Admin.php';
+     $results = (new Admin())->tabulate(null, true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../crud/dist/bootstrap-5.2.3/css/bootstrap.min.css">
+    <style>
+        th, td {
+            vertical-align: middle
+        }
+    </style>
+    <title>Overall Results</title>
+</head>
+<body>
+    <div class="container-fluid mt-3">
+        <table class="table table-bordered">
+            <!-- table header -->
+            <thead>
+                <tr>
+                    <th class="text-center" rowspan="2">
+                        <h1 class="m-0">OFFICIAL RESULTS</h1>
+                    </th>
+                    <?php foreach($results['teams'] as $team_key => $team) { ?>
+                        <th colspan="2" style="color: <?= $team['color'] ?>" class="text-center">
+                            <h6 class="m-0 text-uppercase"><?= $team['color'] ?></h6>
+                            <h4 class="m-0"><?= $team['name'] ?></h4>
+                        </th>
+                    <?php } ?>
+                </tr>
+                <tr>
+                    <?php foreach($results['teams'] as $team_key => $team) { ?>
+                        <th class="text-center">POINTS</th>
+                        <th class="text-center">RANK</th>
+                    <?php } ?>
+                </tr>
+            </thead>
+
+            <!-- table body -->
+            <tbody>
+                <!-- overall -->
+                <tr style="background: yellow">
+                    <td class="fw-bold p-3">
+                        <h4 class="m-0">OVERALL</h4>
+                    </td>
+                    <?php foreach($results['teams'] as $team_key => $team) { ?>
+                        <td align="right">
+                            <h4 class="m-0 fw-normal"><?= number_format($results['teams'][$team_key]['points'], 2) ?></h4>
+                        </td>
+                        <td align="right">
+                            <h4 class="m-0"><?= number_format($results['teams'][$team_key]['rank']['fractional'], 2) ?></h4>
+                        </td>
+                    <?php } ?>
+                </tr>
+
+                <!-- competitions -->
+                <?php foreach($results['competitions'] as $competition_key => $competition) { ?>
+                    <!--divider -->
+                    <tr><td colspan="<?= (1 + sizeof($results['teams'])) ?>"></td></tr>
+
+                    <tr class="table-dark">
+                        <td class="fw-bold p-3">
+                            <h5 class="m-0 text-uppercase"><?= $competition['title'] ?></h5>
+                        </td>
+                        <?php foreach($results['teams'] as $team_key => $team) { ?>
+                            <td align="right">
+                                <h5 class="m-0 fw-normal"><?= number_format($competition['results']['teams'][$team_key]['points'], 2) ?></h5>
+                            </td>
+                            <td align="right">
+                                <h5 class="m-0"><?= number_format($competition['results']['teams'][$team_key]['rank']['fractional'], 2) ?></h5>
+                            </td>
+                        <?php } ?>
+                    </tr>
+
+                    <!-- categories -->
+                    <?php foreach($competition['results']['categories'] as $category_key => $category) { ?>
+                        <tr class="table-secondary">
+                            <td class="fw-bold p-3">
+                                <h6 class="m-0 text-uppercase"><?= $category['title'] ?></h6>
+                            </td>
+                            <?php foreach($results['teams'] as $team_key => $team) { ?>
+                                <td class="py-3" align="right">
+                                    <h6 class="m-0 fw-normal"><?= number_format($category['results']['teams'][$team_key]['points'], 2) ?></h6>
+                                </td>
+                                <td class="py-3" align="right">
+                                    <h6 class="m-0 fw-bold opacity-75"><?= number_format($category['results']['teams'][$team_key]['rank']['fractional'], 2) ?></h6>
+                                </td>
+                            <?php } ?>
+                        </tr>
+
+                        <!-- events -->
+                        <?php foreach($category['results']['events'] as $event_key => $event) { ?>
+                            <tr>
+                                <td class="px-4">
+                                    <p class="m-0 px-4"><?= $event['title'] ?></p>
+                                </td>
+                                <?php foreach($results['teams'] as $team_key => $team) { ?>
+                                    <td align="right">
+                                        <p class="m-0 fw-normal"><?= number_format($event['results']['teams'][$team_key]['points'], 2) ?></p>
+                                    </td>
+                                    <td align="right">
+                                        <p class="m-0 fw-bold opacity-75"><?= number_format($event['results']['teams'][$team_key]['rank']['final']['fractional'], 2) ?></p>
+                                    </td>
+                                <?php } ?>
+                            </tr>
+                        <?php } ?>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="../../crud/dist/bootstrap-5.2.3/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
