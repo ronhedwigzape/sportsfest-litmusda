129760f9aa45e68044d0083f9352fd11413f8086
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-03-20 20:32:55 +0800

    Add line-through text decor for elimination

--- app/crud/eliminations/index.php
+++ app/crud/eliminations/index.php
@@ -1,22 +1,23 @@
 <?php
-    const LOGIN_PAGE_PATH = '../';
-    require_once '../auth.php';
+const LOGIN_PAGE_PATH = '../';
+require_once '../auth.php';
 
-    require_once '../../config/database.php';
-    require_once '../../models/Competition.php';
-    require_once '../../models/Category.php';
-    require_once '../../models/Event.php';
-    require_once '../../models/Team.php';
+require_once '../../config/database.php';
+require_once '../../models/Competition.php';
+require_once '../../models/Category.php';
+require_once '../../models/Event.php';
+require_once '../../models/Team.php';
 
-    $competitions = Competition::all();
-    $categories = Category::all();
-    $teams = Team::all();
+$competitions = Competition::all();
+$categories = Category::all();
+$teams = Team::all();
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../crud/dist/bootstrap-5.2.3/css/bootstrap.min.css">
     <script src="../../crud/dist/vue-3.2.47/vue.global.js"></script>
@@ -26,63 +27,64 @@
 <body>
 <div id="app" class="container mt-3" align="center">
     <h1>Eliminations</h1>
-  <?php foreach ($competitions as $competition) { ?>
-      <h2 class="text-center"><?= $competition->getTitle() ?></h2>
-      <?php foreach ($categories as $category) {?>
-        <hr>
-          <h4><?= $category->getTitle()?></h4>
+    <?php foreach ($competitions as $competition) { ?>
+        <h2 class="text-center"><?= $competition->getTitle() ?></h2>
+        <?php foreach ($categories as $category) { ?>
+            <hr>
+            <h4><?= $category->getTitle() ?></h4>
             <?php
                 $events = Event::all($category->getId());
-                foreach ($events as $event) {
+            foreach ($events as $event) {
                 $event_name = $event->getTitle();
                 $event_id = $event->getId();
             ?>
-            <table class="table table-bordered w-50 text-center">
-                <thead>
+                <table class="table table-bordered w-50 text-center">
+                    <thead>
                     <tr>
                         <th><?php print_r($event_name) ?></th>
                         <th>Action</th>
                     </tr>
-                </thead>
-                <tbody>
-                <?php
-                foreach ($teams as $team) {
-                    $team_name = $team->getName();
-                    $team_id = $team->getId();
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($teams as $team) {
+                        $team_name = $team->getName();
+                        $team_id = $team->getId();
 
-                ?>
-                    <tr>
-                        <td
-                            class="<?= $event->hasTeamBeenEliminated($team) ? 'opacity-50' : '';?>"
-                            :class="{
-                                'opacity-50': (team['isEliminated_<?= $team_id ?>_<?= $event_id ?>'] == true),
-                                'opacity-100': (team['isEliminated_<?= $team_id ?>_<?= $event_id ?>'] == false)
-                            }"
-                        >
-                            <?= $team_name ?>
-                        </td>
-                        <td>
-                            <button
-                                id="action_<?= $team_id ?>_<?= $event_id ?>"
-                                class="btn <?= $event->hasTeamBeenEliminated($team) ? 'btn-primary' : 'btn-danger' ?>"
-                                @click="toggleElimination(<?= $event_id ?>, <?= $team_id ?>)"
+                        ?>
+                        <tr>
+                            <td
+                                id="team_<?= $team_id ?>_<?= $event_id ?>"
+                                class="<?= $event->hasTeamBeenEliminated($team) ? 'opacity-50 text-decoration-line-through' : ''; ?>"
+                                :class="{
+                                    'opacity-50 text-decoration-line-through': (team['isEliminated_<?= $team_id ?>_<?= $event_id ?>'] == true),
+                                    'opacity-100': (team['isEliminated_<?= $team_id ?>_<?= $event_id ?>'] == false)
+                                }"
                             >
-                            <?= $event->hasTeamBeenEliminated($team) ? 'Revive' : 'Eliminate' ?>
-                            </button>
-                        </td>
-                    </tr>
-                <?php } ?>
-                </tbody>
-            </table>
+                                <?= $team_name ?>
+                            </td>
+                            <td>
+                                <button
+                                    id="action_<?= $team_id ?>_<?= $event_id ?>"
+                                    class="btn <?= $event->hasTeamBeenEliminated($team) ? 'btn-primary' : 'btn-danger' ?>"
+                                    @click="toggleElimination(<?= $event_id ?>, <?= $team_id ?>)"
+                                >
+                                    <?= $event->hasTeamBeenEliminated($team) ? 'Revive' : 'Eliminate' ?>
+                                </button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
             <?php } ?>
-      <?php } ?>
-  <?php } ?>
+        <?php } ?>
+    <?php } ?>
 </div>
 </body>
 
 <script>
 
-    const { createApp } = Vue
+    const {createApp} = Vue
 
     createApp({
         data() {
@@ -111,12 +113,15 @@
                                 .html("Revive")
                                 .removeClass("btn-danger")
                                 .addClass("btn-primary");
-                        }
-                        else {
+                            $(`#team_${teamId}_${eventId}`)
+                                .removeClass("text-decoration-line-through");
+                        } else {
                             $(`#action_${teamId}_${eventId}`)
                                 .html("Eliminate")
                                 .removeClass("btn-primary")
                                 .addClass("btn-danger");
+                            $(`#team_${teamId}_${eventId}`)
+                                .addClass("text-decoration-line-through");
                         }
                         console.log(`${jqXHR.status}: ${jqXHR.statusText}`);
                     },
